1ef17b3dd98ff06ff7b1275d9fc52481a7487edc
ictintegratedhub <ictintegratedhub@gmail.com>
2026-07-26 11:47:40 +0100

    update

--- api/hello.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res) => {
-    res.status(200).json({ 
-        message: 'API is working!',
-        env: {
-            hasBingooKey: !!process.env.BINGOO_API_KEY,
-            hasJwtSecret: !!process.env.JWT_SECRET
-        }
-    });
-};
